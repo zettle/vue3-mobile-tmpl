1dd800c1fc9e262af89f5d3ab165a77790863a88
hzt <719925519@qq.com>
2021-10-11 12:42:49 +0800

    feat(约束commit): 约束git的commit信息，规范化

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,11 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // indent: ['error', 4], // js/ts的缩进
     // 'vue/html-indent': ['error', 4], // html的缩进
-    semi: ['error', 'always'] // 分号
+    semi: ['error', 'always'], // 分号
+    "vue/html-closing-bracket-newline": ["warn", { // html的结束符/是否换行
+      "singleline": "never", // 单行 never不换号
+      "multiline": "never" // 多行 never不换号
+    }]
   },
   overrides: [
     {
