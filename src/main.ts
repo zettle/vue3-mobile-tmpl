999ccfef823dcb27e83d3e3b2ef15912deccaf3d
hzt <719925519@qq.com>
2021-11-29 21:30:07 +0800

    fix(editconfig): edit config

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import './utils/rem';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import regGlobalComponent from './utils/regGlobalComponent';
+import regGlobal from './global';
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.use(regGlobalComponent); // 注册vant和自定义的全局组件
+app.use(regGlobal); // 注册全局组件、过滤器
 
 app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 // import viewPage from './viewPage.vue';
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
     redirect: '/entrance/home'
