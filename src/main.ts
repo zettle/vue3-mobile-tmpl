d24c178d4ea9a72bbde93f5dca603e64193aa7b7
zettle <2394024809@qq.com>
2022-01-15 07:30:04 +0800

    feat(优化pinia代码): 优化pinia代码

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import router from './router';
 import regGlobal from './global';
 
 const app = createApp(App);
-app.use(createPinia());
 app.use(router);
+console.log('createPinia');
+app.use(createPinia());
 app.use(regGlobal); // 注册全局组件、过滤器
 
 app.mount('#app');
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,28 +1,55 @@
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { defineSessionStorage } from '@/storage';
 
-const useCounterStore = defineStore('counter', {
-  state: () => ({
-    count: 0,
-    name: '夏明'
-  }),
-  getters: {
-    doubleCount (state) {
-      return state.count * 2;
-    }
-  },
-  actions: {
-    // 同步
-    increment (num: number) {
-      this.count += num;
-    },
-    // 异步
-    incrementAsync (name: string) {
-      setTimeout(() => {
-        this.name = name;
-      }, 1000);
-    }
+// option的写法
+// const useCounterStore = defineStore('counter', {
+//   state: () => ({
+//     count: 0,
+//     name: '夏明'
+//   }),
+//   getters: {
+//     doubleCount (state) {
+//       return state.count * 2;
+//     }
+//   },
+//   actions: {
+//     // 同步
+//     increment (num: number) {
+//       this.count += num;
+//     },
+//     // 异步
+//     incrementAsync (name: string) {
+//       setTimeout(() => {
+//         this.name = name;
+//       }, 1000);
+//     }
+//   }
+// });
+
+// composition的写法推荐
+const useCounterStore = defineStore('counter', function () {
+  const count = ref(0);
+  const name = ref('夏明');
+  const doubleCount = computed(() => count.value * 2);
+
+  function increment (num: number) {
+    count.value += num;
   }
+
+  function incrementAsync (newname: string) {
+    setTimeout(() => {
+      name.value = newname;
+    }, 1000);
+  }
+
+  return {
+    count,
+    name,
+    doubleCount,
+    increment,
+    incrementAsync
+  };
 });
 
 // 持久化
